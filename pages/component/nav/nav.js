05baa84425def2520f4be0f362ad0907a3aa75d7
unknown <unknown@example.com>
2019-12-23 11:08:50 +0800

    Initial Commit

---
+++ pages/component/nav/nav.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    TabCur: 0,
+    scrollLeft:0
+  },
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id-1)*60
+    }),
+    wx.switchTab({
+      url: '../commanag/commanag',
+    })
+  }
+})
